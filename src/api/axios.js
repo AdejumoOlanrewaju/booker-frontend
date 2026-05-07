04bc629324e89127c08712ae74edc10cd3e7bbd8
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2026-05-07 06:12:09 +0100

    fix frontend to backend connection

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5500/api/v1',
 })
 
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
